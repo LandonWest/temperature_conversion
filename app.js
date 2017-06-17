bc541832325e422a9ec249cc64ea2b068a3521e9
Landon West <landonwest5@gmail.com>
2017-06-17 12:38:28 -0700

    refactor app.js; add git rep to package.json

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-let convert = {};
+// let convert = {};
+//
+// convert.cToF = function(celsius) {
+//   if(!Number.isInteger(celsius)) return undefined;
+//   return celsius * 9 / 5 + 32;
+// }
+//
+// convert.fToC = function(fahrenheit) {
+//   if(!Number.isInteger(fahrenheit)) return undefined;
+//   return (fahrenheit - 32) * 5 / 9;
+// }
 
-convert.cToF = function(celsius) {
-  if(!Number.isInteger(celsius)) return undefined;
-  return celsius * 9 / 5 + 32;
-}
-
-convert.fToC = function(fahrenheit) {
-  if(!Number.isInteger(fahrenheit)) return undefined;
-  return (fahrenheit - 32) * 5 / 9;
-}
+// Another way of writing the same thing:
+let convert = {
+  cToF: (celcius) => {
+    if(!Number.isInteger(celcius)) return undefined;
+    return celcius * 9 / 5 + 32;
+  },
+  fToC: (fahrenheit) => {
+    if(!Number.isInteger(fahrenheit)) return undefined;
+    return (fahrenheit - 32) * 5 / 9;
+  }
+};
 
 module.exports = convert;
